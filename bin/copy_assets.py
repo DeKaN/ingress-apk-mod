311bc863797e9b931192d15b6bd9de67ea09494f
Nikolay Kharitonov <o4kapuk@o4kapuk.ru>
2014-02-19 04:54:55 +0400

    Added ttf replacement

--- bin/copy_assets.py
+++ bin/copy_assets.py
@@ -14,6 +14,9 @@ def copy_assets(names=None):
             return
         names = os.listdir(dir1)
 
+    if os.path.isdir('%s/build/assets/fonts' % HOME):
+        shutil.rmtree('%s/app/assets/fonts' % HOME)
+        shutil.copytree('%s/build/assets/fonts' % HOME, '%s/app/assets/fonts' % HOME)
     for n1 in names:
         dir2 = dir1 + '/' + n1
         for n2 in os.listdir(dir2):
@@ -21,6 +24,8 @@ def copy_assets(names=None):
                 if os.path.isdir('%s/app/assets/%s/%s' % (HOME, n2, n1)):
                     shutil.rmtree('%s/app/assets/%s/%s' % (HOME, n2, n1))
                 shutil.copytree(dir2 + '/' + n2, '%s/app/assets/%s/%s' % (HOME, n2, n1))
+    if os.path.isdir('%s/build/assets/i18n' % HOME):
+        shutil.copytree('%s/build/assets/i18n' % HOME, '%s/app/assets/i18n' % HOME)
 
 
 if __name__ == '__main__':
